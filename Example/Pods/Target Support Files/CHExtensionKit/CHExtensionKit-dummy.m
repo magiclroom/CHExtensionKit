f9622b1cdfd99bd7366b3aa56b714e13e70af432
adrian_cai <adrian_cai@askey.com.tw>
2018-09-05 09:07:04 +0800

    change branch

---
+++ Example/Pods/Target Support Files/CHExtensionKit/CHExtensionKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CHExtensionKit : NSObject
+@end
+@implementation PodsDummy_CHExtensionKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-CHExtensionKit_Example/Pods-CHExtensionKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CHExtensionKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CHExtensionKit_Example
+@end
